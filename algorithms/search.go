6b29b9d8414312f2269163e0faf1a7062103b2b7
butuhanov <36451784+butuhanov@users.noreply.github.com>
2019-11-25 11:12:27 +0700

    added search algorithm in ascending sorted list

--- algorithms/search.go
+++ algorithms/search.go
@@ -1,1 +1,20 @@
 package algorithms
+
+// see also: SequentialSearch, BinarySearch
+
+// LinearSearchSorted - search in increasing sorted list
+// when you encounter a greater value element from the increasing sorted list, you stop searching further.
+// Time Complexity: O(n). As we need to traverse the complete list in worst case.
+// Worst case is when your desired element is at the last position of the sorted list. However, in the average case this algorithm is more efficient even though the growth rate is same as unsorted.
+// Space Complexity: O(1). No extra memory is used to allocate the list.
+func LinearSearchSorted(data []int, value int) bool {
+	size := len(data)
+	for i := 0; i < size; i++ {
+		if value == data[i] {
+			return true
+		} else if value < data[i] {
+			return false
+		}
+	}
+	return false
+}
