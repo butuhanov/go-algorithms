d2cc054502c4e5745fad8aa01b7e7e87c2cef935
butuhanov <36451784+butuhanov@users.noreply.github.com>
2019-11-28 17:37:21 +0700

    added exhaustive algorithm for searching element that appears maximum number of times

--- algorithms/search.go
+++ algorithms/search.go
@@ -106,3 +106,24 @@ func FindDublicatesCounting(data []int, intrange int) []int {
 	}
 	return result
 }
+
+// GetMaxAppearing - returns the element that appears maximum number of times in the given list of integers.
+func GetMaxAppearingExhaustive(data []int) int {
+	size := len(data)
+	max := data[0]
+	count := 1
+	maxCount := 1
+	for i := 0; i < size; i++ {
+		count = 1
+		for j := i + 1; j < size; j++ {
+			if data[i] == data[j] {
+				count++
+			}
+		}
+		if count > maxCount {
+			max = data[i]
+			maxCount = count
+		}
+	}
+	return max
+}
