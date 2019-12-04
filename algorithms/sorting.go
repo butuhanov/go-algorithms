5c3ad659f47690db555f284cc621926f3cb1c99e
butuhanov <36451784+butuhanov@users.noreply.github.com>
2019-12-04 11:14:50 +0700

    added modified bubble sorting algorithm

--- algorithms/sorting.go
+++ algorithms/sorting.go
@@ -14,6 +14,22 @@ func BubbleSort(arr []int, comp func(int, int) bool) []int {
 	return arr
 }
 
+// BubbleSortModified is modified bubble sorting algorithm
+func BubbleSortModified(arr []int, comp func(int, int) bool) []int {
+	size := len(arr)
+	swapped := 1
+	for i := 0; i < (size-1) && swapped == 1; i++ {
+		swapped = 0
+		for j := 0; j < size-i-1; j++ {
+			if comp(arr[j], arr[j+1]) {
+				arr[j+1], arr[j] = arr[j], arr[j+1]
+				swapped = 1
+			}
+		}
+	}
+	return arr
+}
+
 // resulting output will be in descending order.
 func less(value1 int, value2 int) bool {
 	return value1 < value2
--- algorithms/sorting_test.go
+++ algorithms/sorting_test.go
@@ -15,10 +15,40 @@ func TestBubbleSort(t *testing.T) {
 
 	var tests = []testelements{
 		{[]int{9, 1, 8, 2, 7, 3, 6, 4, 5}, more, []int{1, 2, 3, 4, 5, 6, 7, 8, 9}},
+		{[]int{9, 1, 8, 2, -7, 3, 6, -4, 5}, more, []int{-7, -4, 1, 2, 3, 5, 6, 8, 9}},
+		{[]int{1, 1, 8, 2, -7, 1, 6, -4, 5}, more, []int{-7, -4, 1, 1, 1, 2, 5, 6, 8}},
+		{[]int{9, -1, 8, 0, 7, 3, -6, 4, 3}, more, []int{-6, -1, 0, 3, 3, 4, 7, 8, 9}},
 	}
 
 	for _, element := range tests {
 		v := BubbleSort(element.values, element.item)
+		if !reflect.DeepEqual(v, element.result) {
+			t.Error(
+				"For", element.values,
+				"expected", element.result,
+				"got", v,
+			)
+		}
+	}
+}
+
+func TestBubbleSortModified(t *testing.T) {
+
+	type testelements struct {
+		values []int
+		item   func(int, int) bool
+		result []int
+	}
+
+	var tests = []testelements{
+		{[]int{9, 1, 8, 2, 7, 3, 6, 4, 5}, more, []int{1, 2, 3, 4, 5, 6, 7, 8, 9}},
+		{[]int{9, 1, 8, 2, -7, 3, 6, -4, 5}, more, []int{-7, -4, 1, 2, 3, 5, 6, 8, 9}},
+		{[]int{1, 1, 8, 2, -7, 1, 6, -4, 5}, more, []int{-7, -4, 1, 1, 1, 2, 5, 6, 8}},
+		{[]int{9, -1, 8, 0, 7, 3, -6, 4, 3}, more, []int{-6, -1, 0, 3, 3, 4, 7, 8, 9}},
+	}
+
+	for _, element := range tests {
+		v := BubbleSortModified(element.values, element.item)
 		if !reflect.DeepEqual(v, element.result) {
 			t.Error(
 				"For", element.values,
