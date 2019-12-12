f608c28920be9fb53bd56e5acc2afe51de9e17fc
butuhanov <36451784+butuhanov@users.noreply.github.com>
2019-12-12 10:49:32 +0700

    added function and test for linked lists

--- algorithms/linked_lists.go
+++ algorithms/linked_lists.go
@@ -1,13 +1,29 @@
 package algorithms
 
-// List is linked list item
-type List struct {
+// LList is linked list item
+type LList struct {
 	head  *NodeLL
 	count int
 }
 
-// Node is an item in the list
+// NodeLL is an item in the list
 type NodeLL struct {
 	value int
 	next  *NodeLL
+}
+
+// LLSize returns size of a given linked list
+func (list *LList) LLSize() int {
+	return list.count
+}
+
+// LLIsEmpty returns true if the linked list is empty
+func (list *LList) LLIsEmpty() bool {
+	return (list.count == 0)
+}
+
+// LLAddHead adds a head to the linked list
+func (list *LList) LLAddHead(value int) {
+	list.head = &NodeLL{value, list.head} //  We need to create a new node with the value passed to the function as argument.
+	list.count++ //  Size of the list is increased by one.
 }
--- algorithms/linked_lists_test.go
+++ algorithms/linked_lists_test.go
@@ -1,1 +1,43 @@
 package algorithms
+
+import (
+	"testing"
+)
+
+func TestLinkedList(t *testing.T) {
+
+	//lst := new(List)
+	lst := &LList{}
+	lst.LLAddHead(1)
+	lst.LLAddHead(2)
+	lst.LLAddHead(3)
+
+	t.Run("Size of List", func(t *testing.T) {
+
+		got := lst.LLSize()
+		want := 3
+
+		if got != want {
+			t.Error(
+				"For", lst,
+				"expected", want,
+				"got", got,
+			)
+		}
+	})
+
+	t.Run("List is empty", func(t *testing.T) {
+
+		got := lst.LLIsEmpty()
+		want := false
+
+		if got != want {
+			t.Error(
+				"For", lst,
+				"expected", want,
+				"got", got,
+			)
+		}
+	})
+
+}
