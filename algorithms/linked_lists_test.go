ad2a3e4f1fc0447578ac35f652c381b97b3e76cb
butuhanov <36451784+butuhanov@users.noreply.github.com>
2019-12-12 11:21:01 +0700

    modify tests

--- algorithms/linked_lists_test.go
+++ algorithms/linked_lists_test.go
@@ -6,38 +6,77 @@ import (
 
 func TestLinkedList(t *testing.T) {
 
+	type testElements struct {
+		lst         *LList
+		sizeResult  int
+		emptyResult bool
+	}
+
 	//lst := new(List)
+
+	lst0 := &LList{}
+
 	lst := &LList{}
 	lst.LLAddHead(1)
 	lst.LLAddHead(2)
 	lst.LLAddHead(3)
 
+	lst1 := &LList{}
+	lst1.LLAddHead(11)
+	lst1.LLAddHead(2)
+	lst1.LLAddHead(32)
+
+	lst2 := &LList{}
+	lst2.LLAddHead(11)
+
+	lst3 := &LList{}
+	lst3.LLAddHead(11)
+	lst3.LLAddHead(-2)
+	lst3.LLAddHead(32)
+	lst3.LLAddHead(35)
+	lst3.LLAddHead(-12)
+
+	var tests = []testElements{
+		{lst0, 0, true},
+		{lst, 3, false},
+		{lst1, 3, false},
+		{lst2, 1, false},
+		{lst3, 5, false},
+	}
+
 	t.Run("Size of List", func(t *testing.T) {
 
-		got := lst.LLSize()
-		want := 3
+		for _, element := range tests {
 
-		if got != want {
-			t.Error(
-				"For", lst,
-				"expected", want,
-				"got", got,
-			)
+			got := element.lst.LLSize() //lst.LLSize()
+			want := element.sizeResult
+
+			if got != want {
+				t.Error(
+					"For", lst,
+					"expected", want,
+					"got", got,
+				)
+			}
 		}
 	})
 
 	t.Run("List is empty", func(t *testing.T) {
 
-		got := lst.LLIsEmpty()
-		want := false
+		for _, element := range tests {
 
-		if got != want {
-			t.Error(
-				"For", lst,
-				"expected", want,
-				"got", got,
-			)
+			got := element.lst.LLIsEmpty() //lst.LLSize()
+			want := element.emptyResult
+
+			if got != want {
+				t.Error(
+					"For", lst,
+					"expected", want,
+					"got", got,
+				)
+			}
 		}
+
 	})
 
 }
